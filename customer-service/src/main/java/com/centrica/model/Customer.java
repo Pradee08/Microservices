c2271ca6919310c67172d4ea4c72073f766a53ee
Pradee08 <66717995+Pradee08@users.noreply.github.com>
2020-06-28 21:09:39 +0530

    Added annotations

--- customer-service/src/main/java/com/centrica/model/Customer.java
+++ customer-service/src/main/java/com/centrica/model/Customer.java
@@ -1,8 +1,12 @@
 package com.centrica.model;
 
 import java.util.List;
+import java.util.Optional;
+import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
@@ -14,6 +18,7 @@ public class Customer {
 	@NotEmpty
 	private String title;
 	@NotEmpty
+	@Size(min=2)
 	private String firstName;
 	@NotEmpty
 	private String lastname;
@@ -23,6 +28,7 @@ public class Customer {
 	private String email;
 	@NotEmpty
 	private List<String> energyAccounts;
+	@Valid
 	private List<PhoneNumber>telephoneNumbers;
 	private CorrespondenceAddress correspondenceAddress;
 
