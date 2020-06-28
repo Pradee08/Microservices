d15f26c4963226ddf8610c5d2544480cdcea5f58
Pradee08 <66717995+Pradee08@users.noreply.github.com>
2020-06-28 20:56:59 +0530

    Update AccountRepository.java

--- account-service/src/main/java/com/centrica/repository/AccountRepository.java
+++ account-service/src/main/java/com/centrica/repository/AccountRepository.java
@@ -1,9 +1,11 @@
 package com.centrica.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.centrica.model.AccountDatabase;
 
 public interface AccountRepository extends JpaRepository<AccountDatabase, String> {
 	Optional<AccountDatabase> findById(String id);
+	Optional<List<AccountDatabase>> findByCustomerId(int customerId);
 }
